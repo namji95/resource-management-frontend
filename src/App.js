61d0ac75ab2347f09497c79d8ae6eb19c274ff99
chapterad <chapterad@naver.com>
2023-07-24 17:34:25 +0900

    Fix : 폼 수정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
 import { useEffect, useState } from 'react';
-import Board from './compontents/Board.js';
-import Input from './compontents/Input.js';
+import Board from './components/Board.js';
+import Input from './components/Input.js';
 import { BrowserRouter as Router, Switch,Route} from 'react-router-dom';
-import Header from './compontents/Header.js';
-import Room from './compontents/Room.js';
+import Header from './components/Header.js';
+import Room from './components/Room.js';
 import {Container, Row, Col, Card, Form, Button,Navbar } from "react-bootstrap";
 
-import ReserveModal from './compontents/ReserveModal'
-import Sidebar from './compontents/Sidebar';
+// import ReserveModal from './compontents/ReserveModal'
+import Sidebar from './components/Sidebar';
 import { Height } from '@mui/icons-material';
 import { height, width } from '@mui/system';
+import InputDeviceForm from './components/Metting/InputDeviceForm';
+import DeviceForm from './components/Device/Device';
+import PageForm from './components/Page9/PageForm';
 
 function App() {
   
@@ -23,15 +26,16 @@ function App() {
         <Route path="/input" element ={<Input></Input>}></Route>
         <Route path='/board' element = {<Board></Board>}></Route>
         <Route path='/room' element = {<Room></Room>}></Route>
+        <Route path='/metting' element = {<InputDeviceForm/>}</Route>
       </Routes> */}
 
       <Header/>
       <div className = "body">
         <Sidebar/>
         <div className= "main">
-          <div style={{border : '1px solid black', height : '100vw',width : '100vw',backgroundColor :'red'}}>
-            메인 컨텐트입니다. test
+          <div style={{border : '1px solid black', height : '100vw',width : '100vw'}}>
             {/* <ReserveModal/> */}
+           <InputDeviceForm></InputDeviceForm>
           </div>
         </div>
         
--- src/components/Metting/InputDeviceForm.js
+++ src/components/Metting/InputDeviceForm.js
@@ -4,79 +4,176 @@ import './FormEx.css';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
-import React, { useState } from 'react';
-import TagTypesExample from './Button';
-import PlaintextExample from './FormEx';
-import MultipleInputsExample from './MultipleInputs';
-import Check from './CheckBox';
-import SelectSizesExample from './SelectForm';
-import FormFileExample from './FileSelect';
-import TextControlsExample from './Texta';
+import React, { useState,useEffect } from 'react';
+import Button from 'react-bootstrap/Button';
+import './Button.css'
+import InputGroup from 'react-bootstrap/InputGroup';
+import axios from 'axios';
 
 function InputDeviceForm() {
   
 const [selectedOption, setSelectedOption] = useState('');
+const [title,setTitle] = useState('');
+const[date,setDate] = useState('');
+const[time,setTime] = useState('');
 
-const handleOptionChange = (event) => {
-  setSelectedOption(event.target.value);
+const handleOptionChange = (e) => {
+  setSelectedOption(e.target.value);
 };
+
+const handleTitle = (e) =>{
+  setTitle(e.target.value);
+}
+const handleDate = (e) =>{
+  setDate(e.target.value);
+}
+const handleTime = (e) =>{
+  setTime(e.target.value);
+}
+const testSubmit =(e) =>{
+  e.preventDefault();
+  axios.post('http://localhost:9000/devices',{
+    title : title,
+    date : date,
+    time : time
+  }).then(response=>{
+    console.log(title);
+    console.log(response.data);
+  })
+}
+
+
 
   return (
     <div>
       
-      <div className="form-container">
-      <fieldset className="fieldset-container">
+      <div className="metting-container">
+      <fieldset className="fieldset-container-meet">
      
-        <TagTypesExample></TagTypesExample>
-        <hr/>
-      <PlaintextExample></PlaintextExample> 
-      <MultipleInputsExample></MultipleInputsExample>
-      
-      <Check></Check>
-      <hr/>
-      <SelectSizesExample></SelectSizesExample>
-      <Form>
       <Form.Group as={Row} className="mb-3">
-        <Form.Label column sm="2">
-         참석자 
+        <Form.Label column sm="2">제목 
         </Form.Label>
         <Col sm="10">
-          <Form.Control type="text" placeholder="이름, 부서를 입력하세요." />
+          <Form.Control type="text" value={title} onChange={handleTitle}   placeholder="제목을 입력하세요." />
         </Col>
       </Form.Group>
-    </Form>     
-    <Form>
-        <Form.Group as={Row} className="mb-3">
-        <Form.Label column sm="2">
-         화상 회의 
-        </Form.Label>
+
+      
+    <InputGroup>
+    <Form.Label column sm="2">
+         일시 
+      </Form.Label>
+      <div className='inputType'>
+     <input type = "Date" onChange={handleDate}></input>
+     <input type = "Time" onChange={handleTime}></input>
+     <text>--</text>
+     <input type = "Date"></input>
+     <input type = "Time"></input>
+     </div>
+    </InputGroup>
+  
+      
+    <Form.Group as={Row} className="mb-3">
+   <Form.Label column sm="2">
+     
+   </Form.Label>
+   <Col sm="6">
+     <Row>
+       <Col sm="2">
+         <Form.Check type="checkbox" label="종일" />
+       </Col>
+       <Col sm="5">
+       </Col>
+     </Row>
+   </Col>
+ </Form.Group>
+      
+      
+      <hr/>
+      <Form>
+    <Form.Group as={Row} className="mb-3">
+      <Form.Label column sm="2">
+       캘린더 
+      </Form.Label>
       <Col sm="10"> 
         <Form.Select aria-label="Default select example">
-      <option>화상 회의 추가</option>
+      <option>[기본]김동민</option>
       <option value="1">One</option>
       <option value="2">Two</option>
       <option value="3">Three</option>
-        </Form.Select>
+    </Form.Select>
       </Col>
     </Form.Group>
+
+    
+  </Form>
+      <Form>
+      <Form.Group as={Row} className="mb-3">
+        <Form.Label column sm="2">
+         이름 
+        </Form.Label>
+        <Col sm="10">
+          <Form.Control type="text" placeholder="예약자 명" />
+        </Col>
+      </Form.Group>
     </Form>
+    <Form.Group as={Row} className="mb-3">
+   <Form.Label column sm="2">
+     수용인원
+   </Form.Label>
+   <Col sm="6">
+     <Row>
+     <Col sm="10"> 
+      <Form.Control placeholder='인원 수를 입력하세요.'></Form.Control>
+      </Col>
+       
+     </Row>
+   </Col>
+ </Form.Group>
+
     <Form.Group as={Row} className="mb-3">
    <Form.Label column sm="2">
      설비
    </Form.Label>
    <Col sm="6">
      <Row>
-       <Col sm="3">
-        <Form.Control type = "text" value="설비 예약">
-
-        </Form.Control>
-       </Col>
+     <Col sm="10"> 
+        <Form.Select aria-label="Default select example">
+      <option>회의실</option>
+      <option value="교육장">교육장</option>
+    </Form.Select>
+      </Col>
+       
      </Row>
    </Col>
  </Form.Group>
  
-      <FormFileExample></FormFileExample>
-      <TextControlsExample></TextControlsExample>
+ <Form.Group as={Row} className="mb-3">
+        <Form.Label column sm="2">장소
+        </Form.Label>
+        
+        <Col sm="10">
+          <Form.Control type="text" placeholder="장소를 입력하세요. " />
+        </Col>
+        <Form.Label column sm="2">
+        </Form.Label>
+        <Col sm = "10">
+          <Form.Control type = "file"/>
+          </Col>
+      </Form.Group>
+     <Form>
+    
+    {/* <Form.Control as="textarea" rows={5} placeholder='메모를 입력하세요.'/> */}
+    <Form.Group as={Row} className="mb-3">
+     <Form.Label column sm="2">
+      메모
+     </Form.Label>
+     <Col sm="10">
+     <Form.Control as="textarea" rows={5} placeholder='메모를 입력하세요.'/>
+     </Col>
+   </Form.Group>
+
+ </Form>
       <Form>
       <Form.Group as={Row} className="mb-3">
         <Form.Label column sm="2">
@@ -84,7 +181,7 @@ const handleOptionChange = (event) => {
         </Form.Label>
 
         <Col sm = "2">
-          <Col sm = "4">
+          <Col sm = "">
         <Form.Check
         type="radio"
         label="공개"
@@ -96,7 +193,7 @@ const handleOptionChange = (event) => {
         </Col>
       
       <Col sm = "2">
-        <Col sm = "5">
+        <Col sm = "">
       <Form.Check
         type="radio"
         label="비공개"
@@ -111,6 +208,11 @@ const handleOptionChange = (event) => {
 
       
     </Form>
+    <InputGroup className='buttongroup'>
+    
+    <Button type="submit" className='submit'onClick={testSubmit}>저장</Button>{' '}
+    <Button as="input" type="button"className='buttoncancle' value="취소" />{' '}
+    </InputGroup>
       </fieldset>
     
       </div>

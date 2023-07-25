a41b8530dd76fb201fa8bdafe96ee6422d3a2724
kimskyyyy <hhhhh823@naver.com>
2023-07-25 16:10:07 +0900

    Fix: 포트번호 수정

---
+++ src/components/company/Test.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Test() {
+    const [testData1, setTestData1] = useState("");
+    const [testData2, setTestData2] = useState("");
+    const [testData3, setTestData3] = useState("");
+
+    return(
+        <>
+        </>
+    )
+}

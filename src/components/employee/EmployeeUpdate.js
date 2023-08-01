d47a3c670e26b491f61e30cff03e3c987488edbe
kimskyyyy <hhhhh823@naver.com>
2023-08-01 17:09:17 +0900

    Feat: 회사, 사원 crud , 로딩

--- src/components/employee/EmployeeUpdate.js
+++ src/components/employee/EmployeeUpdate.js
@@ -1,72 +1,153 @@
-import { Container, Form, Button } from "react-bootstrap";
-import styles from './EmployeeUpdate.module.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function EmployeeUpdate() {
-    return (
-        <Container>
-            <div className="page">
-                <div>
-                    <div className={styles.content_head}>
-                        <div className={styles.title}>
-                            <h3 >
-                                <span>구성원 정보</span>
-                            </h3>
-                            <div style={{height: 20}}>
-                                <button type="button" className={styles.button}>구성원 수정</button>
-                 </div>
+import styles from './css/EmployeeUpdate.module.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+const EmployeeUpdate = ({selectedData, handleCancel, handleEditSubmit}) => {
+
+  const [edited, setEdited] = useState(selectedData);
+  const [empSeq, setEmpSeq] = useState();
+
+  const onCancel = () => {
+    handleCancel();
+  }
+
+  const onEditChange = (e) => {
+    setEdited({
+      ...edited, 
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const onSubmitEdit = (e) => {
+    e.preventDefault();
+    handleEditSubmit(edited);
+
+    // 백엔드
+    console.log(edited.empSeq);
+    setEmpSeq({...selectedData.empSeq})
+
+    axios.post(`http://localhost:8080/api/employee/${Number(edited.empSeq)}`, {
+        empName: edited.empName,
+        empPosition: edited.empPosition,
+        copSeq: edited.copSeq,
+        userSeq: edited.userSeq,
+        authLevel: edited.authLevel,
+    }).then((response) => {
+        console.log(response.data)
+    }).catch((error) => {
+        console.log(error)
+    })
+
+  }
+
+
+  return (
+    <div>
+      <div className={styles.cpFormContainer}>
+        <fieldset className={styles.cpFieldsetContainer}>
+          <Container className={styles.modalHead}>
+            <div><h2>사원 정보</h2></div>
+            <div><button className={styles.buttonSub} onClick={onCancel}>X</button></div>
+          </Container>
+          <br/>
+
+          <form onSubmit={onSubmitEdit}>
+            <div className={styles.editContainer}>
+              <div className={styles.labelInput}><span className={styles.labelItem}>일련번호: {edited.empSeq}</span></div>
+              <div className={styles.labelInput}><span className={styles.labelItem}>사원명: </span>
+                <input 
+                  className={styles.inputItem}
+                  type='text'
+                  name='empName'
+                  value={edited.empName}
+                  onChange={onEditChange}
+                />
+              </div>
+              <div className={styles.labelInput}><span className={styles.labelItem}>직책: </span>
+                <input 
+                  className={styles.inputItem}
+                  type='text'
+                  name='empPosition'
+                  value={edited.empPosition}
+                  onChange={onEditChange}
+                /> 
+              </div>  
+              <div className={styles.labelInput}><span className={styles.labelItem}>인증레벨</span>
+                        <input
+                            className={styles.inputItem}
+                            type="text"
+                            name="authLevel"
+                            placeholder=""
+                            value={edited.authLevel}
+                            onChange={onEditChange}
+                        />
+                        </div>
+                        <div className={styles.labelInput}><span className={styles.labelItem}>(임시)회사일련번호</span>
+                        <input
+                            className={styles.inputItem}
+                            type="text"
+                            name="copSeq"
+                            placeholder="4"
+                            value={edited.copSeq}
+                            onChange={onEditChange}
+                        />
+                        </div>
+                        <div className={styles.labelInput}><span className={styles.labelItem}>(임시)회원일련번호</span>
+                        <input
+                            className={styles.inputItem}
+                            type="text"
+                            name="userSeq"
+                            placeholder="3"
+                            value={edited.userSeq}
+                            onChange={onEditChange}
+                        />
                         </div>
                     </div>
-                <div className={styles.content_body}>
-                    <table class="table table-sm">
-                    {/* <thead class="table-light">
-                        <tr>
-                        <th scope="row">회사</th>
-                        <th scope="row">사번</th>
-                        <th scope="row">ID</th>
-                        <th scope="row">자원 관리</th>
-                        </tr>
-                    </thead> */}
-                    <tbody>
-                        <tr>
-                        <th scope="col">회사</th>
-                        <td>회사</td>
-                        <td>Mark</td>
-                        <td></td>
-                        </tr>
-
-                        <tr>
-                        {/* <th scope="row">사번</th> */}
-                        <td scope="col">사번</td>
-                        <td>Jacob</td>
-                        <td></td>
-                        </tr>
-
-                        <tr>
-                        <th scope="col">ID</th>
-                        <td>Jacob</td>
-                        <td></td>
-                        </tr>
-
-                        <tr>
-                        <th scope="col">자원관리</th>
-                        {/* <td colspan="2">Larry the Bird</td> */}
-                        <td>Larry</td>
-                        <td></td>
-                        </tr>
-                    </tbody>
-                    </table>
-
-                            </div>
-                        </div>
+                <div className={styles.footBtn}>
+              <div><button type='reset' className={styles.buttonCancel} onClick={onCancel}>취소</button></div>
+              <div><button type='submit' className={styles.buttonSave}>저장</button></div>
             </div>
-        
 
 
+          </form>
+        </fieldset>
+      </div>
+    </div>
+  );
 
-
-        </Container>
-
-    )
 }
+
+
+
+//   let data = {};
+//   const copSeq = 1;
+
+
+//   const [companyObj, setCompanyObj] = useState(defaultCompanyObj);
+
+//   const onChangeCompany = (e) => {
+//       let newName = e.target.name;
+//       let newValue = e.target.value;
+//       const newObj = {
+//           ...companyObj,
+//           [newName] : newValue
+//       }
+//       setCompanyObj(newObj);
+//   }
+
+// const onReset = () => {
+//   setCompanyObj(defaultCompanyObj);
+// }
+
+// const closeModal = () => {
+//     props.setModal(!props.modal);
+// }
+
+// const FacilitySaveModal = (event) => {
+//     event.preventDefault();
+
 
 export default EmployeeUpdate;
